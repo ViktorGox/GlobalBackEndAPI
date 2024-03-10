727984b457ec7b2ef60e05fd9f6f980bea87c11b
VitkorKrGo <viktor.krastev03@gmail.com>
2024-03-11 00:00:24 +0100

    Added ITableGeneration and Test inherits from it. Used to create table specific for the entity. Will be changed to dynamic version at later point.

---
+++ GlobalBackEndAPI/DatabaseCreation/ITableGeneration.cs
@@ -0,0 +1,15 @@
+﻿namespace GlobalBackEndAPI.DatabaseCreation
+{
+    // Currently uses raw query, but will be better if it uses some query generator.
+    // https://github.com/ViktorGox/GlobalBackEndAPI/issues/1
+    /// <summary>
+    /// Inherited by classes which will have a table in one of the databases.
+    /// </summary>
+    public interface ITableGeneration
+    {
+        /// <summary>
+        /// Raw query which will be executed and a table will be created from it.
+        /// </summary>
+        string TableGenerationQuery();
+    }
+}
--- GlobalBackEndAPI/RegressionTesting/Models/Test.cs
+++ GlobalBackEndAPI/RegressionTesting/Models/Test.cs
@@ -1,11 +1,24 @@
-﻿namespace GlobalBackEndAPI.RegressionTesting.Models
+﻿using GlobalBackEndAPI.DatabaseCreation;
+
+namespace GlobalBackEndAPI.RegressionTesting.Models
 {
-    public class Test
+    public class Test : ITableGeneration
     {
         public int TestId { get; set; }
         public required string Name { get; set; }
         public required string Description { get; set; }
         public DateTime LastUpdate { get; set; }
         public DateTime CreatedOn { get; set; }
+
+        public string TableGenerationQuery()
+        {
+            return "CREATE TABLE Test (" +
+                   "TestId INT IDENTITY(1,1) PRIMARY KEY," +
+                   "Name NVARCHAR(255) NOT NULL," +
+                   "Description NVARCHAR(255) NULL," +
+                   "LastUpdate DATE DEFAULT GETDATE()," +
+                   "CreatedOn DATE DEFAULT GETDATE()" +
+                   ");";
+        }
     }
 }
