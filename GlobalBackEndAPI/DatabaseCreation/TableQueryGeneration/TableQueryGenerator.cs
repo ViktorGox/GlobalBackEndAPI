52da8f99b52f478951e12e012ef5727d0a9749b3
VitkorKrGo <viktor.krastev03@gmail.com>
2024-03-19 20:03:16 +0100

    TableQuery now generates foregin key alterations.

--- GlobalBackEndAPI/DatabaseCreation/TableQueryGeneration/TableQueryGenerator.cs
+++ GlobalBackEndAPI/DatabaseCreation/TableQueryGeneration/TableQueryGenerator.cs
@@ -8,26 +8,34 @@ namespace GlobalBackEndAPI.DatabaseCreation.TableQueryGenerator
         private readonly ICollection<EntityData> _entities;
         private readonly ITypeAdapter _typeAdapter;
         private readonly ICustomInfoAdapter _customInfoAdapter;
-        private readonly List<string> _tables;
+        private readonly List<string> _queries;
         public TableQueryGenerator(ICollection<EntityData> entityData, ITypeAdapter typeAdapter, ICustomInfoAdapter customInfoAdapter)
         {
             _entities = entityData;
             _typeAdapter = typeAdapter;
             _customInfoAdapter = customInfoAdapter;
-            _tables = new();
+            _queries = new();
         }
 
         public List<string> GenerateMainTables()
         {
+            GenerateTables();
+            AlterForeignKeys();
+            return _queries;
+        }
+
+        private void GenerateTables()
+        {
             foreach (EntityData entity in _entities)
             {
-                string query = GenerateHeader(entity);
                 IReadOnlyList<ColumnData> columnDataList = entity.GetColumnData();
+
+                string query = "CREATE TABLE " + entity.Name + " (";
 
                 for (int i = 0; i < columnDataList.Count; i++)
                 {
                     ColumnData columnData = columnDataList[i];
-                    query += AddColumn(columnData);
+                    query += columnData.Name + " " + _typeAdapter.TypeToString(columnData.Type);
                     query += _customInfoAdapter.Adapt(columnData);
                     if (i < columnDataList.Count - 1)
                     {
@@ -35,19 +43,26 @@ namespace GlobalBackEndAPI.DatabaseCreation.TableQueryGenerator
                     }
                 }
                 query += ")";
-                _tables.Add(query);
+                _queries.Add(query);
             }
-            return _tables;
         }
 
-        private string GenerateHeader(EntityData entityData)
+        private void AlterForeignKeys()
         {
-            return "CREATE TABLE " + entityData.Name + " (";
-        }
+            foreach (EntityData entity in _entities)
+            {
+                IReadOnlyList<ForeignKeyData> foreignKeyData = entity.GetForeignKeys();
 
-        private string AddColumn(ColumnData columnData)
-        {
-            return columnData.Name + " " + _typeAdapter.TypeToString(columnData.Type);
+                for (int i = 0; i < foreignKeyData.Count; i++)
+                {
+                    ForeignKeyData fd = foreignKeyData[i];
+                    string query = "ALTER TABLE " + entity.Name + " ADD FOREIGN KEY (" +
+                        fd.DomesticKey + ") REFERENCES " + fd.ForeignTable + "(" + 
+                        fd.ForeignKey + ") " + fd.CustomRule + ";";
+
+                    _queries.Add(query);
+                }
+            }
         }
     }
 }
