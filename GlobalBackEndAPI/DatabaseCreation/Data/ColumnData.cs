4a81df5dc0d568ee28c20dcdce5a43ef48971521
VitkorKrGo <viktor.krastev03@gmail.com>
2024-03-21 13:38:55 +0100

    Added comments and changed added throws in ColumnData.

--- GlobalBackEndAPI/DatabaseCreation/Data/ColumnData.cs
+++ GlobalBackEndAPI/DatabaseCreation/Data/ColumnData.cs
@@ -12,30 +12,46 @@ namespace GlobalBackEndAPI.DatabaseCreation.Data
         public bool IsPrimaryKey { get; private set; }
         public bool IsForeignKey { get; private set; }
 
+        /// <summary>
+        /// Sets the name of the column. <br></br>
+        /// Does not accept null. <br></br>
+        /// Throws <see cref="NotSupportedException"/> if already assigned <see cref="IsForeignKey"/>.<br></br>
+        /// Does not accept empty, white space only or null value. <br></br>
+        /// </summary>
         public void SetName(string name)
         {
             if (IsForeignKey)
             {
-                CConsole.WriteWarning("Attempting to set name of a column already assigned foreign key!");
-                return;
+                throw new NotSupportedException("Attempting to set name of a column already assigned foreign key.");
             }
-            if (string.IsNullOrWhiteSpace(name)) throw new ArgumentNullException(nameof(name));
+            if (string.IsNullOrWhiteSpace(name))
+            {
+                throw new NotSupportedException("Attempting to set name with empty or null string!");
+            }
             Name = name;
+            return;
         }
 
+        /// <summary>
+        /// Sets the type. Does not accept null.
+        /// </summary>
         public void SetType(Type type)
         {
             ArgumentNullException.ThrowIfNull(type);
             Type = type;
         }
 
+        /// <summary>
+        /// Sets a default value. <br></br>
+        /// Throws <see cref="ArgumentNullException"/> if <paramref name="value"/> is null.<br></br>
+        /// Throws <see cref="NotSupportedException"/> if already assigned <see cref="IsUnique"/>.
+        /// </summary>
         public void SetDefault(object value)
         {
             ArgumentNullException.ThrowIfNull(value);
             if (IsUnique)
             {
-                CConsole.WriteWarning("Attempting to set default when already assigned unique!");
-                return;
+                throw new NotSupportedException("Attempting to set default when already assigned unique!");
             }
             DefaultValue = value;
         }
@@ -45,34 +61,43 @@ namespace GlobalBackEndAPI.DatabaseCreation.Data
             IsNullable = true;
         }
 
+        /// <summary>
+        /// Sets unique requirement. <br></br>
+        /// Throws <see cref="NotSupportedException"/> if already assigned <see cref="DefaultValue"/>.
+        /// </summary>
         public void Unique()
         {
             if (DefaultValue is not null)
             {
-                CConsole.WriteWarning("Attempting to set unique when already assigned default value!");
-                return;
+                throw new NotSupportedException("Attempting to set unique when already assigned default value!");
             }
             IsUnique = true;
         }
 
+        /// <summary>
+        /// Sets as a primary key. <br></br>
+        /// Throws <see cref="NotSupportedException"/> if already assigned <see cref="IsForeignKey"/>.
+        /// </summary>
         public void PrimaryKey()
         {
             if (IsForeignKey)
             {
-                CConsole.WriteWarning("Attempting to set a column as primary key when it was already assigned foreign key!");
-                return;
+                throw new NotSupportedException("Attempting to set a column as primary key when it was already assigned foreign key!");
             }
             IsPrimaryKey = true;
         }
 
+        /// <summary>
+        /// Sets as a foreign key. Changes <see cref="Name"/> to <paramref name="foreignTableKey"/>.<br></br>
+        /// Throws <see cref="NotSupportedException"/> if already assigned <see cref="IsPrimaryKey"/>.
+        /// </summary>
         public void ForeignKey(string foreignTableKey)
         {
             if (IsPrimaryKey)
             {
-                CConsole.WriteWarning("Attempting to set a column as foreign key when it was already assigned primary key!");
-                return;
+                throw new NotSupportedException("Attempting to set a column as foreign key when it was already assigned primary key!");
             }
-            Name = foreignTableKey + "Id";
+            Name = foreignTableKey;
             IsForeignKey = true;
         }
     }
